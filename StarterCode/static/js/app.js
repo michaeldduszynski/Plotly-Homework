8bd99dde8ce5a15df77d17d8688b5fe173ebf39f
michaeldduszynski <76258428+michaeldduszynski@users.noreply.github.com>
2021-05-01 20:03:43 -0500

    Final Commit

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,116 @@
+function getPlots(id) {
+      d3.json("samples.json").then (sampledata =>{
+          console.log(sampledata)
+          var ids = sampledata.samples[0].otu_ids;
+          console.log(ids)
+          var sampleValues =  sampledata.samples[0].sample_values.slice(0,10).reverse();
+          console.log(sampleValues)
+          var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+          console.log (labels)
+      // get only top 10 otu 
+          var OTU_top = ( sampledata.samples[0].otu_ids.slice(0, 10)).reverse();
+          var OTU_id = OTU_top.map(d => "OTU " + d);
+          console.log(`OTU IDS: ${OTU_id}`)
+       // get the top 10 labels 
+          var labels =  sampledata.samples[0].otu_labels.slice(0,10);
+          console.log(`OTU_labels: ${labels}`)
+          var trace = {
+              x: sampleValues,
+              y: OTU_id,
+              text: labels,
+              marker: {
+              color: 'blue'},
+              type:"bar",
+              orientation: "h",
+          };
+          // data variable
+          var data = [trace];
+
+          var layout = {
+              title: "Top 10 OTU",
+              yaxis:{
+                  tickmode:"linear",
+              },
+              margin: {
+                  l: 100,
+                  r: 100,
+                  t: 100,
+                  b: 30
+              }
+          };
+  
+          // create plot
+      Plotly.newPlot("bar", data, layout);
+          var trace1 = {
+              x: sampledata.samples[0].otu_ids,
+              y: sampledata.samples[0].sample_values,
+              mode: "markers",
+              marker: {
+                  size: sampledata.samples[0].sample_values,
+                  color: sampledata.samples[0].otu_ids
+              },
+              text:  sampledata.samples[0].otu_labels
+  
+          };
+  
+          var layout_2 = {
+              xaxis:{title: "OTU ID"},
+              height: 600,
+              width: 1000
+          };
+  
+          // creating data variable 
+          var data1 = [trace1];
+  
+      Plotly.newPlot("bubble", data1, layout_2); 
+      
+      });
+  }  
+
+  function getDemoInfo(id) {
+
+      d3.json("samples.json").then((data)=> {
+
+          var metadata = data.metadata;
+  
+          console.log(metadata)
+  
+   
+         var result = metadata.filter(meta => meta.id.toString() === id)[0];
+        // select demographic panel to put data
+         var demographicInfo = d3.select("#sample-metadata");
+          
+    
+         demographicInfo.html("");
+  
+          Object.entries(result).forEach((key) => {   
+              demographicInfo.append("h5").text(key[0].toUpperCase() + ": " + key[1] + "\n");    
+          });
+      });
+  }
+
+  function optionChanged(id) {
+      getPlots(id);
+      getDemoInfo(id);
+  }
+  
+  
+  function init() {
+      // select dropdown 
+      var dropdown = d3.select("#selDataset");
+  
+  
+      d3.json("samples.json").then((data)=> {
+          console.log(data)
+  
+          data.names.forEach(function(name) {
+              dropdown.append("option").text(name).property("value");
+          });
+  
+        
+          getPlots(data.names[0]);
+          getDemoInfo(data.names[0]);
+      });
+  }
+  
+  init();
